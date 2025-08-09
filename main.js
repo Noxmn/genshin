d84f3c10a58c85b6f3a27fcf8e0daa503993c7c8
NOXMN <saarav652@gmail.com>
2025-08-09 12:40:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-document.getElementById('raidenBtn').addEventListener('click', () => {
-    document.body.style.background = "url('Raiden-Shogun-PNG-HD-Image.png') no-repeat center center/cover";
-});
-
-document.getElementById('nahidaBtn').addEventListener('click', () => {
-    document.body.style.background = "url('Nahida-PNG-Pic.gif') no-repeat center center/cover";
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+        alert(`You clicked on ${card.querySelector("h2").innerText}!`);
+    });
 });

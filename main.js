065a924a219e5344a00935c1f324021e93533de8
NOXMN <saarav652@gmail.com>
2025-08-09 12:53:07 +0530

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("click", () => {
-        alert(`You clicked on ${card.querySelector("h2").innerText}!`);
-    });
-});
